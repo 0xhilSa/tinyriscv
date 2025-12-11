d72a7e0265a5b0b39196c605fe3eb9e63154a687
Sahil <sahilrajwar2004@gmail.com>
2025-12-12 01:31:54 +0530

    we are testing....

--- test.py
+++ test.py
@@ -2,12 +2,9 @@ from tinyriscv import RISCV
 
 riscv = RISCV()
 # riscv.regs[0] = 12 # this will cause the error
-riscv.addi(1, 2, -12)
-riscv.addi(2, 3, 3)
-riscv.sub(3, 2, 1)
-riscv.asm()
-print(riscv.sign_extend(riscv.regs[3], 32))
-print(riscv.regs[3])
+riscv.addi(1, 2, -1) # -> 0 + (-1)
+riscv.addi(2, 3, 2)  # -> 0 + 2
+riscv.add(3, 1, 2)   # -> -1 + 2
+riscv.mul(4, 2, 3)   # -> 2 * 1
 print(riscv.regmap())
-
 #print(riscv.asm(output=True))
