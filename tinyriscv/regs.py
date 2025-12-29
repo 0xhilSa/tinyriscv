137bd6684afb81878e19c7f3f96ce762f6dbccaa
Sahil <sahilrajwar2004@gmail.com>
2025-12-29 17:34:58 +0530

    return custom exception if encountered any

--- tinyriscv/regs.py
+++ tinyriscv/regs.py
@@ -1,14 +1,16 @@
+from tinyriscv import exceptions
+
 class RegFile:
   def __init__(self):
     self.x = [0] * 32
   def __getitem__(self, index:int|slice):
     if isinstance(index, int):
-      if not 0 <= index < len(self.x): raise ValueError(f"invalid register call: {index}")
+      if not 0 <= index < len(self.x): raise exceptions.InvalidRegister(f"invalid register call: {index}")
       return self.x[index]
     elif isinstance(index, slice): return self.x[index.start:index.stop:index.step]
   def __setitem__(self, index, value):
-    if index == 0: raise ValueError(f"can't set the value of register x0(zero)")
-    if not 1 <= index < len(self.x): raise ValueError(f"invalid register call: {index}")
+    if index == 0: raise exceptions.RegisterZeroViolation(f"can't set the value of register x0(zero)")
+    if not 1 <= index < len(self.x): raise exceptions.InvalidRegister(f"invalid register call: {index}")
     self.x[index] = value & 0xfff
   def __iter__(self): return iter(self.x)
   def __len__(self): return len(self.x)
